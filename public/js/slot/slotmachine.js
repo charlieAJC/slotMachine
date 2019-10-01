861a4b77942639b0544e2acddd918d234f8a66e2
Charlie <stu610042002@gmail.com>
2019-10-01 19:25:59 +0800

    slot game bug fix

--- public/js/slot/slotmachine.js
+++ public/js/slot/slotmachine.js
@@ -1,5 +1,3 @@
-// 命名原則
-// CSS能省則省
 $(document).ready(function(){
     // // 按按鈕改變圖片
     // for(i=1;i<7;i++){
@@ -13,24 +11,32 @@ $(document).ready(function(){
     // }
 
     // 設定起始圖案組合
-    var setFlag = 0;
+    var setFlag;
     function rollImg(){
-        for(var i=1;i<4;i++){
-            if(setFlag === 0){
-                var divId = "#img" + i;
-                var newClass = "slot slot" + i
-                $(divId).removeClass();
-                $(divId).addClass(newClass);
-            } else {
-                var divId = "#img" + i;
+        
+        if(setFlag == 0){
+            $("#img1").removeClass();
+            $("#img1").addClass("slot slot1");
+            $("#img2").removeClass();
+            $("#img2").addClass("slot slot2");
+            $("#img3").removeClass();
+            $("#img3").addClass("slot slot3");
+            setFlag = 1;
+        } else {
+            for(var i=1;i<4;i++){
+                var divId = String("#img" + i);
                 var oldClass = $(divId).attr('class');
                 var suffix = oldClass.match(/\d+/);
                 suffix >= 6 ? suffix = 1 : suffix++;
-                var newClass = "slot slot" + suffix
+                suffix = suffix.toString();
+                var newClass = String("slot slot" + suffix)
                 $(divId).removeClass();
                 $(divId).addClass(newClass);
+                
             }
+            alert("");
         }
+        
     };
 
     // 為intervel的名稱，以便關閉時可以呼叫
@@ -42,8 +48,8 @@ $(document).ready(function(){
     $("#casinoShuffle").click(function(){
         // 注意函數名稱不用括弧!
         if(rollFlag === 0){
-            timerFlag = setInterval(rollImg,100);
-            setFlag = 1;
+            setFlag = 0;
+            timerFlag = setInterval(rollImg,50);
             rollFlag = 1;
             $.ajaxSetup({
                 headers: {
