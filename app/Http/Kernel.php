8ea95a089e9faa39babeb04d0ce36d5f4601e61f
chu1015 <chu841015@gmail.com>
2019-10-09 15:47:16 +0800

    middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -60,6 +60,7 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'Permission' => \App\Http\Middleware\PermissionMiddleware::class,
+        'Manager' => \App\Http\Middleware\ManagerMiddleware::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
     ];
 
