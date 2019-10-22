79e186873d56eb3bcbf8339d5b9bb33a3bf749d5
Tina <yuntingmao213@gamil.com>
2019-10-22 17:34:56 +0800

    update pages

--- resources/views/fronted/manager.blade.php
+++ resources/views/fronted/manager.blade.php
@@ -10,6 +10,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>Manager</title>
+    <link rel="Shortcut Icon" type="image/x-icon" href="img/fronted/logo.png" />
 
     <!-- Bootstrap core CSS -->
     <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
