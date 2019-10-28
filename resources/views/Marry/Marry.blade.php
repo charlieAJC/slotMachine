a6052a100d3e60fea7fb273b5fd0da4d903d12a8
Tina <yuntingmao213@gamil.com>
2019-10-28 12:11:59 +0800

    pages update

--- resources/views/Marry/Marry.blade.php
+++ resources/views/Marry/Marry.blade.php
@@ -166,13 +166,15 @@
         </p>
     </div>
     <!-- 投注區域結束 -->
-    <footer class="py-1 bg-dark fixed-bottom" style="opacity:0.9">
+    {{-- <footer class="py-1 bg-dark fixed-bottom" style="opacity:0.9">
         <div class="container">
             <div class="f-copyright" style="text-align:center;color:white;margin:15px;font-size:14px">
                 <span>&copy; Copyright Slot Machine - 2019 </span>
             </div>
         </div>
-    </footer>
+    </footer> --}}
+    @include('fronted.layouts.footer')
+
 
 </body>
 
